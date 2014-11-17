c96d991dc6d10783d17ef86a2118441b2ac0e07f
Alejandro Frias <alejandro.frias@ymail.com>
2014-11-17 14:36:43 -0800

    for loop command

--- code/mind_sketch.py
+++ code/mind_sketch.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re, string
 
 class PromptMindSketchCommand(sublime_plugin.WindowCommand):
 
@@ -18,6 +18,35 @@ class MindSketchCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, line):
 
+		p1 = re.compile(r"^generate for( loop)? from (?P<start>.*?) to (?P<end>.*?)( increment by (?P<amount>.*))?$")
+		t1 ="""for (int i = {start}; i < {end}; i++) {{
+    
+}}"""
+
+		parsers = [p1]
+		templates = [t1]
+
+		i = 0
+		m = parsers[0].match(line)
+		while not(m) and i < len(parsers) - 1:
+			i += 1
+			m = parsers[i].match(line)
+
+		if not(m):
+			print "Command: '" + line + "' did not match any Parser Objects"
+			return
+		# TODO, figure out a way to send format args better
+		code = templates[i].format(start=m.group('start'), end=m.group('end'))
+
 		for region in self.view.sel():
-			self.view.insert(edit, region.begin(), line)
+			(row, column) = self.view.rowcol(region.begin())
+			self.view.insert(edit, region.begin(), reindent(code, column))
+
+def reindent(s, numSpaces):
+	s = string.split(s, '\n')
+	for i in range(1, len(s)):
+		s[i] = (numSpaces * ' ') + s[i]
+	s = string.join(s, '\n')
+	return s
+
 
