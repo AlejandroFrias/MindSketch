e050930f39e719ec002ba4544551f2207a9e2d11
Alejandro Frias <alejandro.frias@ymail.com>
2014-11-18 12:46:32 -0800

    A working plugin for code snippets

--- code/mind_sketch.py
+++ code/mind_sketch.py
@@ -17,15 +17,25 @@ class PromptMindSketchCommand(sublime_plugin.WindowCommand):
 class MindSketchCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, line):
+		parsers = []
+		templates = []
 
-		p1 = re.compile(r"^generate for( loop)? from (?P<start>.*?) to (?P<end>.*?)( increment by (?P<amount>.*))?$")
-		t1 ="""for (int i = {start}; i < {end}; i++) {{
-    
-}}"""
+		parsers += [re.compile(r"^(generate )?for loop from (?P<start>.+?) to (?P<end>.+?) increment by (?P<amount>.+?)$")]
+		templates += ["""for (int i = {0[start]}; i <= {0[end]}; i = i + {0[amount]}) {{
+    $C
+}}"""]
 
-		parsers = [p1]
-		templates = [t1]
+		parsers += [re.compile(r"^(generate )?for loop from (?P<start>.+?) to (?P<end>.+?) decrement by (?P<amount>.+?)$")]
+		templates += ["""for (int i = {0[start]}; i <= {0[end]}; i = i - {0[amount]}) {{
+    $C
+}}"""]
+		parsers += [re.compile(r"^(generate )?for loop from (?P<start>.+?) to (?P<end>.+?)$")]
+		templates += ["""for (int i = {0[start]}; i <= {0[end]}; i++) {{
+    $C
+}}"""]
 
+
+		# In order, try to match each regex from parsers to the input. Stop on first match
 		i = 0
 		m = parsers[0].match(line)
 		while not(m) and i < len(parsers) - 1:
@@ -35,18 +45,23 @@ class MindSketchCommand(sublime_plugin.TextCommand):
 		if not(m):
 			print "Command: '" + line + "' did not match any Parser Objects"
 			return
-		# TODO, figure out a way to send format args better
-		code = templates[i].format(start=m.group('start'), end=m.group('end'))
-
+		
+		# Create and insert the code snippet
+		code = templates[i].format(m.groupdict())
 		for region in self.view.sel():
 			(row, column) = self.view.rowcol(region.begin())
 			self.view.insert(edit, region.begin(), reindent(code, column))
 
+		# Set the cursor to to right place by replacing $C
+		for region in self.view.find_all("$C", sublime.LITERAL):
+			self.view.sel().clear()
+			self.view.sel().add(region)
+			self.view.replace(edit, region, "")
+
+# Indents all lines except the first to match the indentation level of the first line
 def reindent(s, numSpaces):
 	s = string.split(s, '\n')
 	for i in range(1, len(s)):
 		s[i] = (numSpaces * ' ') + s[i]
 	s = string.join(s, '\n')
 	return s
-
-
